411dfff86745e9808aec150a4e3e3836c15a5c56
DeiZHaNger <deiz.in@free.fr>
2016-02-14 17:12:04 +0100

    base de gestion des options

--- langstat.sh
+++ langstat.sh
@@ -2,15 +2,45 @@
 
 alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 tmp='.dzlgsttmp'
+
+if [ $# -lt 1 ]; then
+	echo "argument(s) manquant(s)"
+	exit 1
+fi
+
+# Récupération des options
+while [ "`echo "$1" | cut -c 1`" = "-" ]; do
+	
+	if [ -z `echo "$1" | cut -c 2` ]; then
+		echo "syntaxe : '-' option(s) non spécifiée(s)"
+		exit 1
+	fi
+
+	for option in `echo "$1" | cut -c 2- | sed -E s'/(.)/\1\n/g'`; do
+		case $option in
+			*)
+				echo "-$option : option non valide"
+				;;
+		esac
+	done
+	shift
+done
+
+# Traitement du fichier
 if [ -e "$1" ]; then
+	
+	if [ $# -gt 1 ]; then
+		echo "Les arguments placés après '$1' ont été ignorés"
+	fi
+
 	if [ -e $tmp ]; then
 		rm $tmp
 	fi
 	for char in `echo "$alphabet" | sed -E s'/(.)/\1\n/g'`; do
-		echo -e "$(grep $char $1 | wc -l)\t- $char" >> $tmp
+		echo -e "$(grep $char "$1" | wc -l)\t- $char" >> $tmp
 	done
 	sort -rn $tmp
 	rm $tmp
 else
-	echo "syntax error: ' $0 filename '"
+	echo "nom de fichier invalide ou manquant"
 fi
