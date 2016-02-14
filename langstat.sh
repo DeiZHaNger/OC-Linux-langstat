338ce42d7bd1dba2cc928fe9f6034526292eac47
DeiZHaNger <deiz.in@free.fr>
2016-02-14 23:11:34 +0100

    option -h|--help ajoutée

--- langstat.sh
+++ langstat.sh
@@ -3,12 +3,24 @@
 alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 tmp='.dzlgsttmp'
 
+# Fonction affichage de l'aide rapide
+function quickhelp {
+	echo "Aide rapide -"
+	echo "syntaxe -> ./langstat.sh [option]... nomdufichier (avec extension le cas échéant)"
+	echo "Exemples: "
+	echo "sans option -> ./langstat.sh dico.txt"
+	echo "avec option(s) -> ./langstat.sh -v -p dico.txt"
+       	echo -e "\t\tou ./langstat.sh -pvy dico.txt"
+	echo "Pour plus d'informations veuillez consulter la documentation complète"
+	exit 0
+}
+
+# Récupération des options et gestion des erreurs de saisie
 if [ $# -lt 1 ]; then
 	echo "$0 : argument(s) manquant(s)"
 	exit 1
 fi
 
-# Récupération des options
 while [ "$(echo "$1" | cut -c 1)" = "-" ]; do
 	if [ -z `echo "$1" | cut -c 2` ]; then
 		echo "erreur de syntaxe : '-' option(s) non spécifiée(s)"
@@ -17,9 +29,15 @@ while [ "$(echo "$1" | cut -c 1)" = "-" ]; do
 
 	for option in `echo "$1" | cut -c 2- | sed -E s'/(.)/\1\n/g'`; do
 		case $option in
+			h)
+				quickhelp
+				;;
 			-)
 				longoption=`echo "$1" | cut -c 3-`
 				case $longoption in
+					help)
+						quickhelp
+						;;
 					longoptionname)
 						break	
 						;;
@@ -38,7 +56,7 @@ while [ "$(echo "$1" | cut -c 1)" = "-" ]; do
 	shift
 done
 
-# Traitement du fichier
+# Traitement du fichier passé en argument
 if [ -e "$1" ]; then
 	
 	if [ $# -gt 1 ]; then
