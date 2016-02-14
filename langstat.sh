8f3198c54439d5a0d8ba0b2cdb5c20a3c61b184f
DeiZHaNger <deiz.in@free.fr>
2016-02-14 18:17:09 +0100

    base de gestion des options longues

--- langstat.sh
+++ langstat.sh
@@ -4,22 +4,34 @@ alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 tmp='.dzlgsttmp'
 
 if [ $# -lt 1 ]; then
-	echo "argument(s) manquant(s)"
+	echo "$0 : argument(s) manquant(s)"
 	exit 1
 fi
 
 # Récupération des options
-while [ "`echo "$1" | cut -c 1`" = "-" ]; do
-	
+while [ "$(echo "$1" | cut -c 1)" = "-" ]; do
 	if [ -z `echo "$1" | cut -c 2` ]; then
-		echo "syntaxe : '-' option(s) non spécifiée(s)"
+		echo "erreur de syntaxe : '-' option(s) non spécifiée(s)"
 		exit 1
 	fi
 
 	for option in `echo "$1" | cut -c 2- | sed -E s'/(.)/\1\n/g'`; do
 		case $option in
+			-)
+				longoption=`echo "$1" | cut -c 3-`
+				case $longoption in
+					longoptionname)
+						break	
+						;;
+					*)
+						echo "--$longoption : option longue invalide ou manquante"
+						exit 1
+						;;
+				esac
+				;;
 			*)
-				echo "-$option : option non valide"
+				echo "-$option : option invalide"
+				exit 1
 				;;
 		esac
 	done
